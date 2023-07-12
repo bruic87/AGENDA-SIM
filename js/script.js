d05e184346bb5bca478d2819d2e5a6910783b998
bruic87 <bruic@outlook.com>
2023-07-12 20:33:18 -0300

    'commit da aula de 120723'

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ novoMedico.onclick = function(){
     overlay.classList.add('active');
     drawer.classList.add('active');
 }
+
+fecharCriar.onclick = () => {
+    drawer.classList.remove('active');
+    setTimeout(() => {
+        overlay.classList.remove('active');
+    }, 700);
+}
+
+overlay.onclick = () => {
+    drawer.classList.remove('active');
+    setTimeout(() => {
+        overlay.classList.remove('active');
+    }, 700);
+}
